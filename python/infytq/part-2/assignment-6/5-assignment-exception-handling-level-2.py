477bdd5fb21ce434b111595c15ccae152b262735
Manan Dhiman <manandheman@gmail.com>
2023-07-16 12:10:20 +0530

    exception handling level 2 problem statement correction

--- python/infytq/part-2/assignment-6/5-assignment-exception-handling-level-2.py
+++ python/infytq/part-2/assignment-6/5-assignment-exception-handling-level-2.py
@@ -2,7 +2,7 @@
 A 10-substring of a number is a substring of its digits that sum up to 10.
 
 For example, the 10-substrings of the number 3523014 are:
-3523014, 3523014, 3523014, 3523014
+'352', '523', '5230', '23014'
 
 Write a python function, find_ten_substring(num_str) which accepts a string and returns the list of 10-substrings of that string.
 '''
