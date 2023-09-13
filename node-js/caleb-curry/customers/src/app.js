692a3a078bfe1a1e15f67d5f90060ea4617b258f
Manan Dhiman <manandheman@gmail.com>
2023-09-13 06:58:25 +0530

    Working GET and POST req from MongoDB

--- node-js/caleb-curry/customers/src/app.js
+++ node-js/caleb-curry/customers/src/app.js
@@ -1,11 +1,56 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const mongoose = require('mongoose');
+const Customer = require('./models/customers');
+
+require('dotenv').config();;
+mongoose.set('strictQuery', false);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+const CONNECTION = process.env.CONNECTION;
+
+const customer = new Customer({
+  name: "Caleb V2",
+  industry: "marketing"
+});
 
 app.get('/', (req, res) => {
-  res.send('Express Working with npm start');
+  res.send(customer);
+  res.send("Added")
 });
 
-app.listen(PORT, () => {
-  console.log("App listeining on " + PORT);
+app.post('/', (req, res) => {
+  res.send(req.body  + req.body);
 });
+
+app.get('/api/customers', async(req, res) => {
+  const result = await Customer.find();
+  res.send({"customers": result});
+});
+
+app.post('/api/customers', async(req, res) => {
+  const customer = new Customer({
+    name: req.body.name,
+    industry: req.body.industry
+  });
+  customer.save();
+  res.status(201).json({customer});
+
+});
+
+const start = async() => {
+  try {
+    await mongoose.connect(CONNECTION);
+
+    app.listen(PORT, () => {
+      console.log("App listeining on " + process.env.PORT);
+    });
+  } catch(e) {
+    console.log(e.message);
+  }
+  
+}
+
+start();
